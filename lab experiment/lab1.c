cc1bc058145c51e3d8d86c5717cf08627bd13aa6
shivamkumar8-lang <shivam.kumar8@rungta.org>
2025-12-29 23:00:42 +0530

    Add files via upload

---
+++ lab experiment/lab1.c
@@ -0,0 +1,8 @@
+//first C program print name/age using printf.
+#include <stdio.h>
+int main() {
+    // Print name and age
+    printf("Name: Shivam kumar(11678)\n");
+    printf("Age: 25\n");
+    return 0;
+}
